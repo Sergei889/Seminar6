e8d4f2cca5c12b3174bc639f76e957e96ac1bbba
Sergei.S <sannikov.sergei889@yandex.ru>
2022-12-01 20:58:40 +0300

    Выполнена задача 43 исправлена

--- proekt1/Program.cs
+++ proekt1/Program.cs
@@ -3,6 +3,7 @@
  задаются пользователем.
 
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
 */
 
 Console.WriteLine("Введите значение b1");
@@ -14,6 +15,7 @@ double b2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число k2");
 double k2 = Convert.ToInt32(Console.ReadLine());
 double pointOfIntersection (double b1, double k1, double b2, double k2)
+
 {         
     double x = (b2 - b1) / (k1 - k2);
     double y = k1 * x + b1;
